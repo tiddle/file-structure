27c43564d631cd07f07427fe6631c5033f8ef73c
Carlo Cruz <me@carlocruz.com>
2018-10-14 19:49:32 +1100

    Updates

---
+++ precache-manifest.0eb3cc789cd6e95fa27a713374e32135.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4663c4bc85d86a31d6f5",
+    "url": "/folder-structure/static/js/runtime~main.4663c4bc.js"
+  },
+  {
+    "revision": "59ba3ee7b5772a632cb5",
+    "url": "/folder-structure/static/js/main.59ba3ee7.chunk.js"
+  },
+  {
+    "revision": "dddd9bbb4a12e00f576d",
+    "url": "/folder-structure/static/js/1.dddd9bbb.chunk.js"
+  },
+  {
+    "revision": "59ba3ee7b5772a632cb5",
+    "url": "/folder-structure/static/css/main.8757ef7b.chunk.css"
+  },
+  {
+    "revision": "cd8d879a24356deca558b51aa3395e55",
+    "url": "/folder-structure/index.html"
+  }
+];

cac4cba58d5d937299c30a8fb1a9de3ced850ed9
Carlo Cruz <me@carlocruz.com>
2018-10-14 16:31:05 +1100

    Add getter for remote folder structure

--- src/App.js
+++ src/App.js
@@ -1,98 +1,57 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import Folder from './folder/folder';
 import Size from './total/size';
 import FileCount from './total/file-count';
 import './App.css';
 
-const folderStructure = [
-	{
-		type: 'file',
-		name: 'assurance_interface_multi_tasking.gif',
-		size: 9080
-	},
-	{
-		type: 'folder',
-		name: 'cross-platform',
-		children: [
-			{
-				type: 'folder',
-				name: 'bluetooth',
-				children: [
-					{
-						type: 'folder',
-						name: 'optical',
-						children: [
-							{
-								type: 'folder',
-								name: 'redundant',
-								children: []
-							},
-							{
-								type: 'file',
-								name: 'checking_account.png',
-								size: 78944
-							}
-						]
-					},
-					{
-						type: 'folder',
-						name: 'cross-platform',
-						children: []
-					},
-					{
-						type: 'folder',
-						name: 'redundant',
-						children: [
-							{
-								type: 'file',
-								name: 'panel_e_commerce.mpeg',
-								size: 1494
-							},
-							{
-								type: 'folder',
-								name: '1080p',
-								children: []
-							},
-							{
-								type: 'file',
-								name: 'checking_account_representative.shtml',
-								size: 4307
-							},
-							{
-								type: 'file',
-								name: 'ports_won_portals.mp4',
-								size: 92027
-							}
-						]
-					},
-					{
-						type: 'file',
-						name: 'international.html',
-						size: 6874
-					}
-				]
-			},
-			{
-				type: 'file',
-				name: 'optical.png',
-				size: 23683
-			}
-		]
-	},
-	{
-		type: 'file',
-		name: 'embrace_deposit.png',
-		size: 18499
-	}
-];
-
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			folderStructure: []
+		};
+	}
+
+	componentDidMount() {
+		axios
+			.get('https://chal-locdrmwqia.now.sh/')
+			.then(response => {
+				this.setState({
+					folderStructure: response.data.data
+				});
+			})
+			.catch(error => {
+				this.setState({
+					error: error
+				});
+			});
+	}
+
 	render() {
 		return (
 			<div className="App">
-				<Folder folderStructure={folderStructure} closed={false} />
-				<Size folderStructure={folderStructure} />
-				<FileCount folderStructure={folderStructure} />
+				{this.state.folderStructure.length > 0 && (
+					<>
+						<Folder
+							folderStructure={this.state.folderStructure}
+							closed={false}
+						/>
+						<Size folderStructure={this.state.folderStructure} />
+						<FileCount
+							folderStructure={this.state.folderStructure}
+						/>
+					</>
+				)}
+				{this.state.error && <pre>{this.state.error.message}</pre>}
+				<p>
+					Please note that there are sometimes errors in the console
+					about duplicate keys. I'm ok with this error as the api
+					spits out file names that are the same on the same level
+					which isn't really possible in real life.
+				</p>
 			</div>
 		);
 	}
